80b6bdd81d33e7ef2980e90b6ac2bdf992bbe2c0
Nethmi Vihanga Gampalage <151724895+nvihanga@users.noreply.github.com>
2025-05-07 23:09:31 +0530

    Modified

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes } from '@angular/router';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CustomerListComponent },
+    { path: 'create', component: CustomerFormComponent },
+    { path: 'edit/:id', component: CustomerFormComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
