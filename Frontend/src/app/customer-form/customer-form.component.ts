d41dfd4413ce6aea14c388030d6acc6f0bdf3783
Nethmi Vihanga Gampalage <151724895+nvihanga@users.noreply.github.com>
2025-05-08 10:17:07 +0530

    Modified

--- Frontend/src/app/customer-form/customer-form.component.ts
+++ Frontend/src/app/customer-form/customer-form.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-customer-form',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.css'
 })
 export class CustomerFormComponent {
   customer = {
-    id: '',
     name: '',
     age: '',
     nic: '',
     address: ''
   };
 
-  onSubmit() {
-    console.log('Customer Data:', this.customer);
-    alert('Customer submitted successfully!');
+  formSubmitted = false;
+  successMessage = '';
+
+  onSubmit(form: NgForm) {
+    this.formSubmitted = true;
+
+    if (form.invalid) return;
+
+    console.log('Form submitted:', this.customer);
+
+     alert('Customer added successfully');
+
+    form.resetForm();
+    this.formSubmitted = false;
   }
 }
