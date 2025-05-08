a97d104eaf4306a3178f0f440f29760832a49904
Nethmi Vihanga Gampalage <151724895+nvihanga@users.noreply.github.com>
2025-05-08 09:18:18 +0530

    Created Customer Form

--- Frontend/src/app/customer-form/customer-form.component.ts
+++ Frontend/src/app/customer-form/customer-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.css'
 })
 export class CustomerFormComponent {
+  customer = {
+    id: '',
+    name: '',
+    age: '',
+    nic: '',
+    address: ''
+  };
 
+  onSubmit() {
+    console.log('Customer Data:', this.customer);
+    alert('Customer submitted successfully!');
+  }
 }
