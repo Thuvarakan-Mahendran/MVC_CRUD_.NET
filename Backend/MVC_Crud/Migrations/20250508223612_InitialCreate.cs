406464b05361f166380fee692f693d9ce67c034f
Thuvarakan-Mahendran <mahendranthuvarakan2001@gmail.com>
2025-05-09 05:37:22 +0530

    Backend added with CORS

--- Backend/MVC_Crud/Migrations/20250508223612_InitialCreate.cs
+++ Backend/MVC_Crud/Migrations/20250508223612_InitialCreate.cs
@@ -17,7 +17,9 @@ namespace MVC_Crud.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Age = table.Column<int>(type: "int", nullable: false)
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    NIC = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
                 },
                 constraints: table =>
                 {
--- Backend/MVC_Crud/Program.cs
+++ Backend/MVC_Crud/Program.cs
@@ -16,6 +16,22 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+        Console.WriteLine("Migration applied successfully");
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occured while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
