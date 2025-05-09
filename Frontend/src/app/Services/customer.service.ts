24b6032977257480ac7ee26f3c5bd67df222df9c
ksdkaveesha <ksdkaveesha@gmail.com>
2025-05-09 10:45:16 +0530

    update some design

--- Frontend/src/app/Services/customer.service.ts
+++ Frontend/src/app/Services/customer.service.ts
@@ -5,25 +5,25 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CustomerService {
   private selectedCustomer: any;
-  private apiUrl = 'http://localhost:3000/api/customers';
+  // private apiUrl = 'http://localhost:3000/api/customers';
 
-  constructor(private http: HttpClient) { }
+  // constructor(private http: HttpClient) { }
 
-  getCustomers(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
-  }
+  // getCustomers(): Observable<any[]> {
+  //   return this.http.get<any[]>(this.apiUrl);
+  // }
 
-  addCustomer(customer: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, customer);
-  }
+  // addCustomer(customer: any): Observable<any> {
+  //   return this.http.post<any>(this.apiUrl, customer);
+  // }
 
-  updateCustomer(customer: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${customer.id}`, customer);
-  }
+  // updateCustomer(customer: any): Observable<any> {
+  //   return this.http.put<any>(`${this.apiUrl}/${customer.id}`, customer);
+  // }
 
-  deleteCustomer(customerId: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${customerId}`);
-  }
+  // deleteCustomer(customerId: number): Observable<any> {
+  //   return this.http.delete<any>(`${this.apiUrl}/${customerId}`);
+  // }
 
   setCustomer(customer: any) {
     this.selectedCustomer = customer;
